41c0db000f74049a864e0dd2ced2237a865c3dd1
Aleksey Rodionov <rodial.pro@gmail.com>
2021-09-25 22:17:14 +0400

    Blue heat map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -55,10 +55,10 @@ export default defineComponent({
           intensityOfMidpoint: 0.2,
           // JSON описание градиента.
           gradient: {
-            0.1: 'rgba(128, 255, 0, 0.7)',
-            0.2: 'rgba(255, 255, 0, 0.8)',
-            0.7: 'rgba(234, 72, 58, 0.9)',
-            1.0: 'rgba(162, 36, 25, 1)'
+            0.1: 'rgba(176, 224, 230, 0.7)',
+            0.2: 'rgba(135, 206, 235, 0.8)',
+            0.7: 'rgba(0, 191, 255, 0.9)',
+            1.0: 'rgba(30, 144, 255, 1)'
           }
         });
         heatmap.setMap(map);
